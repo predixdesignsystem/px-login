eb79a35b2c905e2a3422a2dc9e0eab855fa025fa
Greg Stroup <gregory.stroup@ge.com>
2017-03-02 14:27:54 -0800

    remove fancy es6 stuff

--- test/px-login-custom-tests.js
+++ test/px-login-custom-tests.js
@@ -1,37 +1,37 @@
 // This is the wrapper for custom tests, called upon web components ready state
-let runCustomTests = () => {
+var runCustomTests = function() {
   // Place any setup steps like variable declaration and initialization here
-  let server;
-  const fakeUserInfo = '{"user_name": "FromServer"}';
+  var server;
+  var fakeUserInfo = '{"user_name": "FromServer"}';
 
-  setup(() => {
+  setup(function() {
     server = sinon.fakeServer.create();
     server.respondWith('GET', '/userinfo', [200, {"Content-Type": "application/json"}, fakeUserInfo]);
     server.autoRespond = true;
   });
 
-  teardown(() => {
+  teardown(function() {
     server.restore();
   });
 
   // This is the placeholder suite to place custom tests in
   // Use testCase(options) for a more convenient setup of the test cases
-  suite('Custom Automation Tests for px-login', () => {
-    setup(() => {
-      const loginEl = Polymer.dom(document).querySelector('px-login');
+  suite('Custom Automation Tests for px-login', function() {
+    setup(function() {
+      var loginEl = Polymer.dom(document).querySelector('px-login');
       loginEl.userName = null;
       loginEl.userInfo = null;
     });
 
-    test('Sign in button shows by default', (done) => {
-      const loginEl = Polymer.dom(document).querySelector('px-login'),
+    test('Sign in button shows by default', function(done) {
+      var loginEl = Polymer.dom(document).querySelector('px-login'),
           loginButton = Polymer.dom(loginEl.root).querySelector('#loginButton');
       assert.include(loginButton.textContent, 'Sign In');
       done();
     });
 
-    test('Username shows when set', (done) => {
-      let loginEl = Polymer.dom(document).querySelector('px-login'),
+    test('Username shows when set', function(done) {
+      var loginEl = Polymer.dom(document).querySelector('px-login'),
           userNameButton;
       loginEl.userName = 'Greg S';
       userNameButton = Polymer.dom(loginEl.root).querySelector('#userNameButton');
@@ -39,8 +39,8 @@ let runCustomTests = () => {
       done();
     });
 
-    test('Clicking username shows logout popover', (done) => {
-      let loginEl = Polymer.dom(document).querySelector('px-login'),
+    test('Clicking username shows logout popover', function(done) {
+      var loginEl = Polymer.dom(document).querySelector('px-login'),
           userNameButton,
           popoverWrapper = document.querySelector('#popoverWrapper');
 
@@ -48,40 +48,40 @@ let runCustomTests = () => {
       userNameButton = Polymer.dom(loginEl.root).querySelector('#userNameButton');
       expect(popoverWrapper.classList.contains('fadeFromHidden')).to.equal(false);
       userNameButton.click();
-      flush(() => {
+      flush(function() {
         popoverWrapper = document.querySelector('#popoverWrapper');
         expect(popoverWrapper.classList.contains('fadeFromHidden')).to.equal(true);
         done();
       });
     });
 
-    test('Clicking login changes window.location to loginUrl', (done) => {
-      let loginEl = Polymer.dom(document).querySelector('px-login'),
+    test('Clicking login changes window.location to loginUrl', function(done) {
+      var loginEl = Polymer.dom(document).querySelector('px-login'),
           loginButton = Polymer.dom(loginEl.root).querySelector('#loginButton');
       loginEl.loginUrl = '#login';
       loginButton.click();
-      flush(() => {
+      flush(function() {
         assert.equal('#login', window.location.hash);
         done();
       });
     });
 
-    test('Userinfo from server is stored in properties', (done) => {
-      const loginEl = Polymer.dom(document).querySelector('px-login'),
+    test('Userinfo from server is stored in properties', function(done) {
+      var loginEl = Polymer.dom(document).querySelector('px-login'),
         userInfoAjax = Polymer.dom(loginEl.root).querySelector('#userInfoAjax');
       loginEl.userName = null;
-      let request = userInfoAjax.generateRequest();
+      var request = userInfoAjax.generateRequest();
       server.respond();
 
-      flush(() => {
+      flush(function() {
         assert.equal(loginEl.userName, JSON.parse(fakeUserInfo).user_name);
         assert.deepEqual(loginEl.userInfo, JSON.parse(fakeUserInfo));
         done();
       });      
     });
 
-    test('Clicking logout clears properties, and changes window.location to logoutUrl', (done) => {
-      let loginEl = Polymer.dom(document).querySelector('px-login'),
+    test('Clicking logout clears properties, and changes window.location to logoutUrl', function(done) {
+      var loginEl = Polymer.dom(document).querySelector('px-login'),
           userNameButton,
           logoutButton,
           popoverWrapper = document.querySelector('#popoverWrapper');
@@ -90,13 +90,13 @@ let runCustomTests = () => {
       loginEl.logoutUrl = '#logout';
       userNameButton = Polymer.dom(loginEl.root).querySelector('#userNameButton');
       userNameButton.click();
-      flush(() => {
+      flush(function() {
         popoverWrapper = document.querySelector('#popoverWrapper');
         logoutButton = document.querySelector('#logoutButton');
         logoutButton.click();
       });
 
-      flush(() => {
+      flush(function() {
         assert.isNull(loginEl.userName);
         assert.isNull(loginEl.userInfo);
         assert.equal('#logout', window.location.hash);
